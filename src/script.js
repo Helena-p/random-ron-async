fc2061149c35d490c0f1060e06f272d1d0380939
Helena <helenasleterkesto@gmail.com>
2021-06-22 09:32:12 +0200

    Add comments

--- src/script.js
+++ src/script.js
@@ -1,8 +1,12 @@
 const quoteBtn = document.querySelector("#get-quote");
 const showQuote = document.querySelector("#display-quote");
 
+// Hold previously used quotes
 let quotes = [];
 
+// If response return with error
+// notify user with error message
+// and display a warning with error status in console
 async function getQuote() {
     try {
         let response = await fetch(
@@ -15,18 +19,26 @@ async function getQuote() {
 
         let data = await response.json();
 
+        // If list of quotes exceed 49, remove the first quote
         if (quotes.length > 49) {
             quotes.shift();
         }
-
+        // If quote exist in list, get new quote
         if (quotes.includes(data[0])) {
             getQuote();
             return;
         }
+
+        // Display quote and then move recently used quote to list of quotes
         showQuote.textContent = data[0];
         quotes.push(data[0]);
+
+        // // If response return with error
+        // notify user with error message
+        // and display a warning with error status in console
     } catch (error) {
         showQuote.textContent = `Oops..page not found. Please try again later.`;
+        console.warn(error);
     }
 }
 
