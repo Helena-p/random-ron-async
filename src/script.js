53811f46018468711081c6669211209ba9b1c531
Helena <helenasleterkesto@gmail.com>
2021-10-22 12:54:21 +0200

    Refactoring code

--- src/script.js
+++ src/script.js
@@ -1,50 +1,38 @@
-const quoteBtn = document.querySelector("#get-quote");
-const showQuote = document.querySelector("#display-quote");
+"use strict";
 
-// Hold previously used quotes
-let quotes = [];
+const displayQuote = document.querySelector("[data-quote]");
+const button = document.querySelector('[type="button"]');
+const endpoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+const quotes = [];
 
-// If response return with error
-// notify user with error message
-// and display a warning with error status in console
-async function getQuote() {
+const responseHandler = (response) => {
+    if (!response.ok) throw Error(response.statusText);
+    return response.json();
+};
+
+const renderQuote = (data) => {
+    quotes.includes(data[0])
+        ? getQuote()
+        : (displayQuote.textContent = `‟ ${data[0]}`);
+    if (quotes.length >= 50) quotes.shift();
+    quotes.push(data[0]);
+};
+
+const errorHandler = (error) => {
+    displayQuote.textContent = `${error}, No quotes available at this time`;
+};
+
+const getQuote = async (event) => {
     try {
-        let response = await fetch(
-            "https://ron-swanson-quotes.herokuapp.com/v2/quotes"
-        );
-        // check response value to be true
-        if (!response.ok) throw response.status;
-
-        // Get Json data from response
-        let data = await response.json();
-        if (!data) throw "No data!";
-
-        // If list of quotes exceed 49, remove the first quote
-        if (quotes.length > 49) {
-            quotes.shift();
-        }
-        // If quote exist in list, get new quote
-        if (quotes.includes(data[0])) {
-            getQuote();
-            return;
-        }
-
-        // Display quote and then move recently used quote to list of quotes
-        showQuote.textContent = data[0];
-        quotes.push(data[0]);
-
-        // // If response return with error
-        // notify user with error message
-        // and display a warning with error status in console
+        const response = await fetch(endpoint);
+        const data = await responseHandler(response);
+        const renderData = await renderQuote(data);
+        return renderData;
     } catch (error) {
-        showQuote.textContent = `Oops..page not found. Please try again later.`;
-        console.warn(error);
+        errorHandler(error);
+        console.error(error);
     }
-}
+};
 
-// Run function getQuote on page load
-// and diplay quote to user
-getQuote();
-
-// Fetch new quote when user click on button
-quoteBtn.addEventListener("click", getQuote);
+window.addEventListener("load", getQuote);
+button.addEventListener("click", getQuote);
